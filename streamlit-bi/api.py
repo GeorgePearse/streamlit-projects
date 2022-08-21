060ccb17db115bd5a9a6f1d7dee75c5f62c05aa3
George Pearse <georgep@eng-d-ml-george-02.behold.ai>
2022-08-21 14:22:21 +0000

    API now working perfectly

--- streamlit-bi/api.py
+++ streamlit-bi/api.py
@@ -4,14 +4,10 @@ import pandas as pd
 
 app = FastAPI()
 
-@app.get("/query/{query_name}")
+@app.get("/query/{query_name}/{json_format}")
 async def root(
-    query_name: str = Path(
-        title="Query name of the query to run", 
-        default='hospital_counts'),
-    json_format: str = Path(
-        title='Format of returned json', 
-        default='index')
+    query_name: str,
+    json_format: str
 ):
     analysis_conn = create_connection("core.db")
     queries_conn = create_connection("queries.db")
@@ -19,7 +15,6 @@ async def root(
     select query_contents from queries where query_name = '{query_name}'
     """).fetchall())[0] 
     selected_query = selected_query[0]
-    return selected_query
-    # results_df = sql_to_df(analysis_conn, selected_query)
-    # return results_df.to_json(orient=json_format)
+    results_df = sql_to_df(analysis_conn, selected_query)
+    return results_df.to_json(orient=json_format)
 
--- streamlit-bi/pages/call_api.py
+++ streamlit-bi/pages/call_api.py
@@ -10,7 +10,7 @@ selected_query = st.selectbox('Query to Execute', list(queries_df['query_name'])
 json_formats = ['split', 'records', 'index', 'columns', 'values', 'table']
 selected_json_format = st.selectbox('Json Format', json_formats)
 
-url = f'http://127.0.0.1:8001/query/{selected_query}?json_format={selected_json_format}'
+url = f'http://127.0.0.1:8002/query/{selected_query}/{selected_json_format}'
 
 code = f"""
 url = '{url}'
