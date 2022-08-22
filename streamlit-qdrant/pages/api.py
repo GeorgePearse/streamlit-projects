d90fbbb8d7dbba873178a21e5af72ac709a987b0
George Pearse <georgep@eng-d-ml-george-02.behold.ai>
2022-08-22 09:06:49 +0000

    removed unused imports

--- streamlit-qdrant/pages/api.py
+++ streamlit-qdrant/pages/api.py
@@ -4,6 +4,8 @@ import pandas as pd
 import requests
 import streamlit as st
 
+from utils import get_json_formats_select_box
+
 st.write("# API Interface")
 
 saved_queries = "./pages/saved_queries/"
@@ -12,8 +14,7 @@ examples_clean = [example.replace(".json", "") for example in examples]
 selected_query = st.selectbox("Examples", examples_clean)
 
 json_formats = ["split", "records", "index", "columns", "values", "table"]
-selected_json_format = st.selectbox("Json Format", json_formats)
-
+selected_json_format = get_json_formats_select_box()
 
 url = f"http://127.0.0.1:8004/query/{selected_query}/{selected_json_format}"
 
--- streamlit-qdrant/utils.py
+++ streamlit-qdrant/utils.py
@@ -1,81 +1,14 @@
 import boto3
 import io
-import pydicom
-from PIL import Image
 import streamlit as st
-import sqlite3
 from pandas.core.frame import DataFrame
 import pandas as pd
 import logging
 
 
-def create_connection(db_file):
-    """create a database connection to the SQLite database
-        specified by the db_file
-    :param db_file: database file
-    :return: Connection object or None
-    """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-    except Exception as e:
-        st.write(e)
-
-    return conn
-
-
-def instantiate_queries_table():
-    try:
-        conn = create_connection("queries.db")
-        conn.execute(
-            """
-            create table queries (query_name varchar, query_contents varchar);
-        """,
-        )
-        conn.commit()
-        # always go for default index names when possible
-        conn.execute(
-            """
-            CREATE UNIQUE INDEX IF NOT EXISTS
-            ON queries(query_name);
-        """,
-        )
-        conn.commit()
-
-    except:
-        pass
-
-
-def sql_to_df(conn, query):
-    """
-    Pandas does this well, maybe just copy there's
-    """
-    results = conn.execute(query)
-
-    cols = [column[0] for column in results.description]
-    results_df = pd.DataFrame.from_records(
-        data=results.fetchall(),
-        columns=cols,
-    )
-    return results_df
-
-
-def delete_component(conn, query: str, column_name: str, delete_statement: str):
-    """
-    Pandas does this well, maybe just copy there's
-    """
-
-    instantiate_queries_table()
-    queries_df = sql_to_df(conn, query)
-    st.dataframe(queries_df)
-    query_to_delete = st.selectbox("Query to Delete", list(queries_df[column_name]))
-
-    if st.button("Delete"):
-        st.write(delete_statement)
-        conn.execute(delete_statement)
-        conn.commit()
-        queries_df = sql_to_df(conn, query)
-        st.dataframe(queries_df)
+def get_json_formats_select_box():
+    json_formats = ["split", "records", "index", "columns", "values", "table"]
+    return st.selectbox("Json Format", json_formats)
 
 
 def get_image_direct_from_s3(bucket: str, key: str):
@@ -99,16 +32,29 @@ def get_s3_metadata(ml_dataset, response_df: DataFrame) -> DataFrame:
             {"new_s3_info": 1},
         ),
     )
-    s3_metadata["payload.s3_bucket"] = s3_metadata["new_s3_info"].apply(lambda x: x.get("PNG_256", {}).get("Bucket"))
-    s3_metadata["payload.s3_key"] = s3_metadata["new_s3_info"].apply(lambda x: x.get("PNG_256", {}).get("Key"))
+    s3_metadata["payload.s3_bucket"] = s3_metadata["new_s3_info"].apply(
+        lambda x: x.get("PNG_256", {}).get("Bucket"),
+    )
+    s3_metadata["payload.s3_key"] = s3_metadata["new_s3_info"].apply(
+        lambda x: x.get("PNG_256", {}).get("Key"),
+    )
 
-    new_response_df = pd.merge(response_df, s3_metadata, left_on="id", right_on="_id", how="inner")
+    new_response_df = pd.merge(
+        response_df,
+        s3_metadata,
+        left_on="id",
+        right_on="_id",
+        how="inner",
+    )
     return new_response_df
 
 
 # TODO: This method of passing the mongo collection through is a mess and
 # shouldn't be needed, it's only needed for the s3 details at the minute
-def prep_display_content(ml_dataset, response_df):  # make sure things stay in sync if there's an error loading the png
+def prep_display_content(
+    ml_dataset,
+    response_df,
+):  # make sure things stay in sync if there's an error loading the png
     """Retrieves the image for each instance_uuid at the same time as parsing the
     relevant metadata so that they remain in sync ready for display
     (image retrieval can fail in some cases)
@@ -123,7 +69,11 @@ def prep_display_content(ml_dataset, response_df):  # make sure things stay in s
     metadata_list = []
     with st.spinner("One second.."):
         for counter, (instance_uuid, bucket, key) in enumerate(
-            zip(response_df["id"], response_df["payload.s3_bucket"], response_df["payload.s3_key"]),
+            zip(
+                response_df["id"],
+                response_df["payload.s3_bucket"],
+                response_df["payload.s3_key"],
+            ),
         ):
             try:
                 key = key.replace("256", "1024")  # tmp fix
